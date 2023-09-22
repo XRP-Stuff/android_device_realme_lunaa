4323bbe6f023c23f74dd1770664653fc7d58f8d6
rk134 <rahul@aospa.co>
2023-09-22 07:40:36 +0000

    lunaa: init: Setup widevine certificate copying * this fixes L1 on lunaa.
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,10 @@ PRODUCT_SYSTEM_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/display_id_4630946741972277890.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946741972277890.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    init.oplus.widevine.rc
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.st \
